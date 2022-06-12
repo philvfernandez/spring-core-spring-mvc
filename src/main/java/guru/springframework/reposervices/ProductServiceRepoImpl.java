32782c038991013939103355603f79e1952ba325
Phil Fernandez <philvfernandez@gmail.com>
2022-06-11 21:32:04 -0600

    Add Product CRUD repositories.

---
+++ src/main/java/guru/springframework/reposervices/ProductServiceRepoImpl.java
@@ -0,0 +1,45 @@
+package guru.springframework.reposervices;
+
+import guru.springframework.domain.Product;
+import guru.springframework.respositories.ProductRepository;
+import guru.springframework.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Profile({"springdatajpa", "jpadao"})
+public class ProductServiceRepoImpl implements ProductService {
+
+    private ProductRepository productRepository;
+
+    @Autowired
+    public void setProductRepository(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Override
+    public List<?> listAll() {
+        List<Product> products = new ArrayList<>();
+        productRepository.findAll().forEach(products::add);  //fun with Java 8
+        return products;
+    }
+
+    @Override
+    public Product getById(Integer id) {
+        return productRepository.findOne(id);
+    }
+
+    @Override
+    public Product saveOrUpdate(Product domainObject) {
+        return productRepository.save(domainObject);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.delete(id);
+    }
+}
